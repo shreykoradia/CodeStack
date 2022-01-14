998d53fcafca6b84e2c5f5186d2bd53c0887fb94
shreykoradia <shrey.koradia@yahoo.com>
2022-01-14 12:37:39 +0530

    done with the login component

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
+import {Button , Grid , makeStyles , Typography } from "@material-ui/core"
+import {TextField} from '@material-ui/core'
+//import {FormControl} from '@material-ui/core
+
+const styles={
+    display: 'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop :'5%',
+    marginLeft:'2%',
+    maxWidth:'100vw',
+    flexDirection:'row'
+}
+
+const useStyles = makeStyles({
+    root: {
+            // display:'flex',
+            border: 0,
+            background: '#212121',
+            color: 'white',
+            '&:hover':{
+                background: '#212121',
+                variant:'raised'
+            },
+            padding: "9px 18px",
+            height:'3rem',
+            width:'14rem',
+            margin:'1rem'       
+    },
+
+    
+});
+
+
 
 function Login() {
+    const classes = useStyles();
     return (
         <>
-             <h1>Hello world still login page is building</h1>  
+        <div style={styles}>
+            <Grid container direction={"column"} spacing={2} justifyContent='center' alignItems='center' >
+            <Grid item>
+            <Typography   paragraph={false} align="left" gutterBottom={true} variant="h4" noWrap={false}>CodeStack</Typography>
+            </Grid>
+             <Grid item>
+             <Typography   paragraph={true} align="center" gutterBottom={true} variant="overline" noWrap={false}>
+                Be the 1% of the 1% , Codestack where code is the chaos ! 
+            </Typography>
+             </Grid>   
+            <Grid item>
+             <TextField label="Email" placeholder='Email'type="email" variant="outlined"  required  />
+           </Grid>
+           <Grid item>
+             <TextField label="Password" type="password" placeholder='Password' variant="outlined" required />
+           </Grid>
+           {/* <Grid item>
+           <Checkbox
+            icon={<BookmarkBorderIcon />}
+            checkedIcon={< BookmarkIcon />}/>
+            <Typography> Remember me</Typography>
+           </Grid> */}
+           <Grid item>
+           <Button variant="contained" href="#submit" color={"#000000"}  size='large' className={classes.root}>
+                Login
+            </Button>
+            </Grid>
+           </Grid>
+
+        </div>
+            
         </>
     )
 }
