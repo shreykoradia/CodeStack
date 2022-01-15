4f8dfd73d5506f1aad997d9c48887a684c5e9b58
shreykoradia <shrey.koradia@yahoo.com>
2022-01-15 15:39:14 +0530

    refactored the firebaseApp name

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
  import firebase from 'firebase/app'
  import "firebase/auth"
 
- const app = firebase.initializeApp({
+ const firebaseApp = firebase.initializeApp({
     apiKey: process.env.REACT_FIREBASE_API_KEY  ,
     authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_FIREBASE_PROJECT_ID,
@@ -11,4 +11,4 @@
  })
  
  export const auth = app.auth()
- export default app 
+ export default firebaseApp
