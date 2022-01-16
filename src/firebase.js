7d84513fd1a61d5f99ce46be5c360eaf4aa7f4f3
shreykoradia <shrey.koradia@yahoo.com>
2022-01-16 16:59:03 +0530

    refactored the code on firebase feature branch

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,23 @@
-<<<<<<< HEAD
- import firebase from 'firebase/app'
- import "firebase/auth"
+// <<<<<<< HEAD
+//  import firebase from 'firebase/app'
+//  import "firebase/auth"
 
- const firebaseApp = firebase.initializeApp({
-    apiKey: process.env.REACT_FIREBASE_API_KEY  ,
-    authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_FIREBASE_APP_ID
- })
+//  const firebaseApp = firebase.initializeApp({
+//     apiKey: process.env.REACT_FIREBASE_API_KEY  ,
+//     authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_FIREBASE_APP_ID
+//  })
  
- export const auth = firebaseApp.auth()
- export default firebaseApp
-=======
+//  export const auth = firebaseApp.auth()
+//  export default firebaseApp
+
+
+// ========================================================//
+
+
 // import firebase from 'firebase/compat/app';
 // import 'firebase/compat/auth';
 
@@ -50,4 +54,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 
 export default app
->>>>>>> firebasefeature
+
+//>>>>>>> firebasefeature
