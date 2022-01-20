e380033955b1885e78b3bc847e5be0614b9b12a7
shreykoradia <shrey.koradia@yahoo.com>
2022-01-20 10:56:25 +0530

    still need to work on the sidebar design and code

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Sidebar from './Sidebar/Sidebar'
 
 function Dashboard() {
     return (
-        <div>
-            Hey I am currently gettin updated welcome here Chief !
-        </div>
+        <>
+            {/* sidebar */}
+                <Sidebar />
+            {/* feed for code-reviews */}
+            {/*  App documentation or widget  */}
+        </>
     )
 }
 
---
+++ src/component/Dashboard/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div>
+            Hello building 
+        </div>
+    )
+}
+
+export default Sidebar
